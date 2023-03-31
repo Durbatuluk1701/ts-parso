0bae417baaa76d6cfe9c1d67e04c8e7c820b5810
Will Thomas <30wthomas@gmail.com>
2023-03-30 22:37:45 -0500

    Adding index.js to tracked files

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Parser_1 = require("./Parser");
+const Tokenizer_1 = require("./Tokenizer");
+exports.Tokenize = Tokenizer_1.Tokenize;
+exports.Parser = Parser_1.Parser;
+//# sourceMappingURL=index.js.map
